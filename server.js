376cb2e24995e55a671b88add3189a514ae78398
Austin Arias <thenandback@gmail.com>
2015-10-11 21:53:21 -0700

    save for deploy

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
 var port = process.env.PORT || 3000;
-var Adjective = require('./lib/adjective.js');
+
+var Adjective = require('./lib/word.js').adjective;
+var Verb = require('./lib/word.js').verb;
+var Noun = require('./lib/word.js').noun;
 var getRandomWord = require('./lib/getRandomWord.js');
 
 var adjective = new Adjective();
+var verb = new Verb();
+var noun = new Noun();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
@@ -15,34 +20,6 @@ app.use(bodyparser.urlencoded({
 }));
 
 app.use(express.static(__dirname + '/app/'));
-
-var Verb = function() {
-  this.sleeping = true;
-  this.skiing = true;
-  this.flopped = true;
-  this.waiting = true;
-  this.plotting = true;
-  this.burping = true;
-  this.sneezing = true;
-  this.flying = true;
-  this.snoozing = true;
-  this.dancing = true;
-};
-var verb = new Verb();
-
-var Noun = function() {
-  this.ninjas = true;
-  this.cowboys = true;
-  this.bears = true;
-  this.beats = true;
-  this.alligators = true;
-  this.hamsters = true;
-  this.otters = true;
-  this.helicopters = true;
-  this.unicorns = true;
-  this.rhymes = true;
-};
-var noun = new Noun();
 
 function postWord (word, wordObject) {
   if (wordObject.hasOwnProperty(word)) {
@@ -68,6 +45,16 @@ app.get('/noun', function(req, res) {
 
 app.post('/adjective', function(req, res) {
   var word = postWord(req.body.word, adjective);
+  res.json(word);
+});
+
+app.post('/verb', function(req, res) {
+  var word = postWord(req.body.word, verb);
+  res.json(word);
+});
+
+app.post('/noun', function(req, res) {
+  var word = postWord(req.body.word, noun);
   res.json(word);
 });
 
